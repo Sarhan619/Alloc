7f635113895673c3f0035a5117698fb6aa6b29b2
Sarhan619 <60283941+Sarhan619@users.noreply.github.com>
2025-08-20 15:56:48 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // important when serving from a custom root domain
+  build: {
+    outDir: 'dist', // default, but explicit is good
+    emptyOutDir: true
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
