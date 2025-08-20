72bbe3100825f7c8d516295790a4907f98dc4756
Sarhan619 <60283941+Sarhan619@users.noreply.github.com>
2025-08-20 15:57:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // important when serving from a custom root domain
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'dist/index.html', dest: '.', rename: '404.html' }
+      ]
+    })
+  ],
+  base: '/',
   build: {
-    outDir: 'dist', // default, but explicit is good
+    outDir: 'dist',
     emptyOutDir: true
   },
   optimizeDeps: {
